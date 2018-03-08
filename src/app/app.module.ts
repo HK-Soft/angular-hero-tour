c00274b52bda24f93eb8e7e2b8afbd84e22f9e25
Abdeldjalil <hn.abdeldjalil@gmail.com>
2018-03-08 12:54:09 +0100

    Hero get and update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component'
-
+import { HttpClientModule } from '@angular/common/http' ;
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,11 @@ import { DashboardComponent } from './dashboard/dashboard.component'
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [
     HeroService,
